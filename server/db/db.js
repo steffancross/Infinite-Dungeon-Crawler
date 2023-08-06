9852ea7443e80755e3142ab8c38a47c48f605d3f
Steffan Cross <steffancross@berkeley.edu>
2023-08-06 16:08:26 -0700

    deploy db link updated

--- server/db/db.js
+++ server/db/db.js
@@ -22,13 +22,16 @@ if (process.env.DATABASE_URL) {
 }
 
 //local db
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   config
+// );
+// module.exports = db;
+
+//deployment db
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL ||
+    "postgres://infinite_dungeon_crawler_user:SLP4gzFOxE4De582FnKDeqqWoyQtngqk@dpg-cj82dkljeehc73fhgn30-a/infinite_dungeon_crawler",
   config
 );
 module.exports = db;
-
-//deployment db
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || 'postgresql://idcwewilldeploy:v2_44MJa_3L7kmM7XwWcAwxxwVTb33Nj@db.bit.io:5432/idcwewilldeploy/i-d-c?ssl=true', config)
-// module.exports = db
